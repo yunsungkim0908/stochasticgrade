0e0ea5c645b880d7f81a3709f03db05004e7a0dd
jadongeathers <jadongeathers@gmail.com>
2024-04-26 13:04:38 -0700

    Bug fixing

--- stochasticgrade/choose_n.py
+++ stochasticgrade/choose_n.py
@@ -3,52 +3,15 @@ import configparser
 import json
 import numpy as np
 import os
+import shutil
 
 from stochasticgrade.constants import DATA_DIR
 from stochasticgrade.grade import StochasticGrade
 from stochasticgrade.score import *
-
-
-def initialize(file_path, qid):
-    """
-    Initialize the error directory and the FAR for running the script.
-    
-    file_path (str): the path to the closest error program, as a .py file
-    qid (str):       the question ID
-    """
-
-    print('\n- - - - - INITIALIZATION - - - - -\n')
-    
-    # Create the directory for the closest error program
-    print('Creating the directory for the closest error program.')
-    if not os.path.isdir(os.path.join(DATA_DIR, qid, 'closest_error', 'closest_error')):
-        os.makedirs(os.path.join(DATA_DIR, qid, 'closest_error', 'closest_error'))
-        
-    # Move the read program into the directory using the sid `closest_error`
-    if not file_path.endswith('.py'):
-        raise ValueError('Please provide the path to a valid .py file for the closest error program.')
-    with open(file_path) as f:
-        error_program = f.read()
-    with open(os.path.join(DATA_DIR, qid, 'closest_error', 'closest_error', 'program.py'), 'w') as f:
-        f.write(error_program)
-    
-    # Request a FAR to add to the config file
-    print('\nPlease select the desirable false acceptance rate for the closest error program.')
-    print('This is a float, e.g. 0.1.')
-    far = input('FAR: ')
-     
-    # Update the config file
-    config = configparser.ConfigParser()
-    if not os.path.isfile(os.path.join(DATA_DIR, qid, 'config.ini')):   
-        raise ValueError('Please ensure the config.ini is in your qid directory!')
-    config.read(os.path.join(DATA_DIR, qid, 'config.ini'))
-    config['Parameters']['far'] = far
-    with open(os.path.join(DATA_DIR, qid, 'config.ini'), 'w') as f:
-        config.write(f)
         
 
-def choose_n(qid, false_acceptance_rate, false_rejection_rate, num_soln_samples,  
-             scorer, dtype, func_name, min_n=200, max_n=500000, M=10, test_label='', test_args=[]):
+def choose_n(qid, false_acceptance_rate, false_rejection_rate, n_soln_samples,  
+             scorer, dtype, func_name, min_n, max_n, M=1000):
     """
     Determine the sample size that satisfies the provided FAR when run on
     the closest error program. Out of `M` identical error programs, only a
@@ -57,7 +20,7 @@ def choose_n(qid, false_acceptance_rate, false_rejection_rate, num_soln_samples,
     qid (str):                     the question ID
     false_acceptance_rate (float): the false acceptance rate for the error program
     false_rejection_rate (float):  the false rejection rate for the correct programs
-    num_soln_samples (int):        the number of solution samples
+    n_soln_samples (int):          the number of solution samples
     scorer (Scorer):               the scoring function to use
     dtype (str):                   the data type of the problem
     func_name (str):               the name of the function from which samples are collected
@@ -79,31 +42,47 @@ def choose_n(qid, false_acceptance_rate, false_rejection_rate, num_soln_samples,
         curr *= 2
     sizes.append(max_n)
     
-    # The ID for the closest error program
-    sid = 'closest_error'
+    # Create the IDs for the closest error program
+    sids = [f'closest_error_{i+1}' for i in range(M)]
+    for sid in sids:
+        sid_dir = os.path.join(DATA_DIR, qid, 'setup', sid)
+        if not os.path.isdir(sid_dir):
+            os.makedirs(sid_dir)
+        src = os.path.join(DATA_DIR, qid, 'setup', 'closest_error', 'program.py')
+        dst = os.path.join(sid_dir, 'program.py')
+        shutil.copy(src, dst)
     
     # Execute the grading algorithm for each sample size until the FAR is satisfied
+    found = False
     for size in sizes:
         print(f'Testing size {size}')
         best_n = size
         algorithm = StochasticGrade(qid, scorer, [size], false_rejection_rate, dtype, func_name,
-                                    num_soln_samples=num_soln_samples, test_label=test_label, test_args=test_args)
+                                    n_soln_samples=n_soln_samples)
         # Run the grading algorithm on the error program M times
-        false_acceptances = 0
-        for _ in tqdm(range(M)):
-            result = algorithm.grade(sid)
-            accepted = result[0]
-            if accepted: 
-                false_acceptances += 1
+        false_acceptances = 0    
+        results = algorithm.grade_parallel(sids)
+        for sid in results:
+            false_acceptances += 1 if results[sid][0] else 0
         calculated_far = false_acceptances / M
         
         # Determine if the calculated FAR is sufficient or if we need more samples
         if calculated_far <= false_acceptance_rate:
             print(f'Calculated FAR: {calculated_far} is within the tolerance of {false_acceptance_rate}.\n')
             print(f'The best value of N: {best_n}\n')
-            return best_n
+            found = True
+            break
         print(f'Calculated FAR: {calculated_far} is beyond the tolerance of {false_acceptance_rate}.')
-    print(f'\nNo best_n found within the sample size maximum. Returning max_n of {max_n}.\n')
+    
+    if not found:
+        print(f'\nNo best_n found within the sample size maximum. Returning max_n of {max_n}.\n')
+    
+    print('Cleaning up directories.')
+    for sid in sids:
+        sid_dir = os.path.join(DATA_DIR, qid, 'setup', sid)
+        shutil.rmtree(sid_dir)
+    print('Success!\n\n')
+    
     return best_n
 
 
@@ -111,39 +90,38 @@ if __name__ == '__main__':
     
     # Load command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('qid', type=str)
-    parser.add_argument('file_path', type=str)
+    parser.add_argument('qid', type=str, help='the question ID')
+    parser.add_argument('--far', type=float, default=0.1, help='the false acceptance rate')
+    parser.add_argument('--M', type=int, default=1000, 
+                        help='the number of times to execute the grading algorithm to check FAR')
     args = parser.parse_args()
     
     qid = args.qid
-    file_path = args.file_path
-    
-    # Initialize the error program directory and FAR
-    initialize(file_path, qid)
+    far = args.far
+    M = args.M
     
     # Load config file
     config = configparser.ConfigParser()
-    if os.path.isfile(os.path.join(DATA_DIR, qid, 'config.ini')):   
-        config.read(os.path.join(DATA_DIR, qid, 'config.ini'))
+    if os.path.isfile(os.path.join(DATA_DIR, qid, 'setup', 'config.ini')):   
+        config.read(os.path.join(DATA_DIR, qid, 'setup', 'config.ini'))
        
     # Load model parameters
     dtype = config['Parameters']['dtype']
-    num_soln_samples = int(config['Parameters']['num_soln_samples'])
+    n_soln_samples = int(config['Parameters']['n_soln_samples'])
     min_n = int(config['Parameters']['min_n'])
     max_n = int(config['Parameters']['max_n'])
     scorer_name = config['Parameters']['scorer']
     scorer_map = make_scorer_map()
     scorer = scorer_map[scorer_name]
     func_name = config['Parameters']['func_name']
-    far = float(config['Parameters']['far'])
     frr = float(config['Parameters']['frr'])
                
     # Run choose_n and save the best selection for the number of samples that obey the FAR
     best_n = choose_n(
-        qid, far, frr, num_soln_samples, scorer, dtype, func_name, min_n=min_n, max_n=max_n
+        qid, far, frr, n_soln_samples, scorer, dtype, func_name, min_n, max_n, M=M
     )
-    with open(os.path.join(DATA_DIR, qid, 'closest_error', f'best_n_far={far}.txt'), 'w') as f:
-        f.write(f'best_n = {best_n}')
+    with open(os.path.join(DATA_DIR, qid, 'setup', f'best_n_far={far}.txt'), 'w') as f:
+        f.write(f'best_n={best_n}')
     
                 
             
