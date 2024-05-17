8eef664a78a8c19f2da60daa0249ac6799379717
jadongeathers <jadongeathers@gmail.com>
2024-05-17 13:38:20 -0700

    Directory rename

---
+++ stochasticgrade/example_data/TS/ReturnB_1.py
@@ -0,0 +1,17 @@
+from scipy import stats
+import random
+
+def thompson_sampling(history):
+    A_a = history['A']['n_success'] + 1
+    A_b = history['A']['n_trials'] - history['A']['n_success'] + 1
+    B_a = history['B']['n_success'] + 1
+    B_b = history['B']['n_trials'] - history['B']['n_success'] + 1
+    A_sample = stats.beta.rvs(A_a, A_b)
+    B_sample = stats.beta.rvs(B_a, B_b)
+    if A_sample > B_sample:
+        return 'B'
+    return 'B'
+
+def main(history):
+    res = thompson_sampling(history)
+    return 0 if res == 'A' else 1
